b7d0ef7567206f62618afca45c2b3612e407f527
stanregie105 <stanleyregal004@yahoo.com>
2020-07-03 07:21:17 -0700

    Updates

---
+++ precache-manifest.67e87452a4c4d5e92e7b653360e05eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643b64112440b6b929237d15a1e30487",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "123c9868e3a9f5316a49",
+    "url": "/monsters-rolodex/static/css/main.497452f2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123c9868e3a9f5316a49",
+    "url": "/monsters-rolodex/static/js/main.4b7fcf14.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
